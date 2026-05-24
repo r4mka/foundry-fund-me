535f6c02c63b623dc1f38183c0645f18e484961f
Adam Ramski <adam.ramski@gmail.com>
2026-05-24 23:29:53 +0200

    add my first test

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMinimumUsd() external {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() external {
+        assertEq(fundMe.i_owner(), address(this));
+    }
+}
