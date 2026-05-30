ebb0c7093440f32e5e237bfd21cc2732261f4518
Adam Ramski <adam.ramski@gmail.com>
2026-05-30 21:23:53 +0200

    add HelperConfig.s.sol

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -3,13 +3,16 @@ pragma solidity ^0.8.18;
 
 import { Script } from "forge-std/Script.sol";
 import { FundMe } from "../src/FundMe.sol";
+import { HelperConfig } from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
     function run() external returns (FundMe) {
+        // Before startBroadcast -> Not a real tx
+        HelperConfig helperConfig = new HelperConfig();
+        address priceFeed = helperConfig.activeNetworkConfig();
+
         vm.startBroadcast();
-        // Sepolia ETH / USD Address
-        // https://docs.chain.link/data-feeds/price-feeds/addresses
-        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundMe = new FundMe(priceFeed);
         vm.stopBroadcast();
         return fundMe;
     }
